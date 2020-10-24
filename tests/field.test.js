e4c193db290cf41d40eba4b6f02a3d6aeefa69e2
Douglas Kelly <dkelly@bamboohr.com>
2020-10-23 21:23:54 -0600

    making a test for field

--- tests/field.test.js
+++ tests/field.test.js
@@ -1,7 +1,29 @@
 import { Field } from '../app/field';
 
-test("field constructor", () => {
-    let field = new Field(2, 6, 9);
-    expect(field.getWidth()).toBe(10);
-    expect(field.getLength()).toBe(9);
+describe('field tests', () => {
+
+    test("field constructor", () => {
+        let field = new Field(2, 6, 9);
+        expect(field.getWidth()).toBe(10);
+        expect(field.getLength()).toBe(9);
+    });
+
+    test('field drawing', () => {
+        let field = new Field(2, 6, 9);
+        let beginPath = jest.fn();
+        let moveTo = jest.fn();
+        let lineTo = jest.fn();
+        let stroke = jest.fn();
+        let canvasRenderingContext2D = {
+            beginPath: beginPath,
+            moveTo: moveTo,
+            lineTo: lineTo,
+            stroke: stroke,
+        };
+        field.draw(canvasRenderingContext2D);
+        expect(beginPath).toHaveBeenCalledTimes(2);
+        expect(beginPath).toHaveBeenCalledTimes(2);
+        expect(beginPath).toHaveBeenCalledTimes(2);
+        expect(beginPath).toHaveBeenCalledTimes(2);
+    })
 });
